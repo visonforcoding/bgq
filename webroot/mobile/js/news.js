3575ab9235091e4f3a84b46c2d3b27e153676c45
xiaofeng <xiaofeng@35271882-e656-49ff-8e86-f188e04c67cb>
2016-05-05 15:49:46 +0000

    规范化 demo

---
+++ webroot/mobile/js/news.js
@@ -0,0 +1,41 @@
+var news = function(){
+    var opt={
+
+    };
+
+    $.extend(this, opt);
+}
+
+news.prototype.init = function(){
+    this.setDet();
+    this.bindEvent();
+};
+
+news.prototype.setDet = function(){
+
+};
+
+news.prototype.bindEvent = function(){
+    $('body').on('tap', function(e){
+        var target = e.srcElement || e.target, em=target, i=1;
+        while(em && !em.id && i<=3){ em = em.parentNode; i++;}
+        if(!em || !em.id) return;
+        switch(em.id){
+            case 'imageViewer': case 'fullImg':
+                //do();
+            break;
+            case 'blackCover':
+                //do();
+                break;
+            case 'detailClosePC':
+                //do();
+                break;
+            case 'goTop':
+                window.scroll(0,0);
+                e.preventDefault();
+                break;
+        }
+    });
+};
+
+(new news()).init();
