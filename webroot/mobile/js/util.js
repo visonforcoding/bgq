09587d843dc1843c6992634e410ac3f4fb23ac49
xiaofeng <xiaofeng@35271882-e656-49ff-8e86-f188e04c67cb>
2016-05-05 15:49:46 +0000

    规范化 demo

---
+++ webroot/mobile/js/util.js
@@ -0,0 +1,40 @@
+$.util = {
+    //返回id查找原素，没有找到时，防止为空，会构造一个
+    id:function(str){
+        return document.getElementById(str) || document.createElement('span');
+    },
+
+    //
+    alert:function(str, t){
+        $.util.idObj('#alert_text').html(str);
+        $('#alert_plan').show();
+        setTimeout(function(){$('#alert_plan').hide();}, t||3000);
+    },
+
+
+    //循环轮播
+    loop:function(opt){
+        return new scroll(opt);
+    },
+
+    //轮播图  传入的都是
+    loopImg:function(fatherDom, child, tab) {
+        $.util.loop({
+            tp : 'img', //图片img或是文字text
+            //min : 5,
+            loadImg:true,
+            moveDom : fatherDom, // eg: $('#loopImgUl')
+            moveChild: child, //$('#loopImgUl li')
+            tab:tab, //$('#loopImgBar li')
+            //loopScroll:(this.loopImg.length > 1 ? true:false),
+            lockScrY:true,
+            imgInitLazy:1000,
+            //enableTransX:true,
+            index: 1,
+            fun:function(index){
+            }
+        });
+    }
+
+
+};
