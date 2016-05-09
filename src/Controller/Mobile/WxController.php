332c733aebafb004b4b3252782a78cf807889301
caowenpeng <caowenpeng1990@126.com>
2016-05-09 09:33:32 +0000

    微信登录 微信头像

--- src/Controller/Mobile/WxController.php
+++ src/Controller/Mobile/WxController.php
@@ -72,109 +72,17 @@ class WxController extends AppController {
                 return $this->redirect(['controller' => 'index', 'action' => 'index']);
             } else {
                 $this->request->session()->write('reg.wx_openid', $open_id);
+                $access_token = json_decode($response->body())->access_token;
+                $wx_user_url = 'https://api.weixin.qq.com/sns/userinfo?access_token=' . $access_token . '&openid=' . $open_id . '&lang=zh_CN';
+                $res = $httpClient->get($wx_user_url);
+                if ($res->isOk()) {
+                    $userinfo = json_decode($res->body()); //微信用户信息
+                    $headimgurl = $userinfo->headimgurl;
+                    $this->request->session()->write('avatar',$headimgurl);
+                }
                 return $this->redirect(['controller' => 'user', 'action' => 'wxBindPhone']);
             }
-            $access_token = json_decode($response->body())->access_token;
-            $wx_user_url = 'https://api.weixin.qq.com/sns/userinfo?access_token=' . $access_token . '&openid=' . $open_id . '&lang=zh_CN';
-            $res = $httpClient->get($wx_user_url);
-            if ($res->isOk()) {
-                $userinfo = json_decode($res->body()); //微信用户信息
-                $open_id = $userinfo->openid;
-                //$headimgurl = $userinfo->headimgurl;
-            }
         }
-    }
-
-    /**
-     * View method
-     *
-     * @param string|null $id User id.
-     * @return \Cake\Network\Response|null
-     * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
-     */
-    public function view($id = null) {
-        $user = $this->User->get($id, [
-            'contain' => ['Industries', 'Cities']
-        ]);
-
-        $this->set('user', $user);
-        $this->set('_serialize', ['user']);
-    }
-
-    /**
-     * Add method
-     *
-     * @return \Cake\Network\Response|void Redirects on successful add, renders view otherwise.
-     */
-    public function register() {
-        //发邮件
-        $user = $this->User->newEntity();
-        if ($this->request->is('post')) {
-            $user = $this->User->patchEntity($user, $this->request->data);
-            if ($this->User->save($user)) {
-                $this->Util->ajaxReturn(true, '添加成功');
-            } else {
-                $errors = $user->errors();
-                $this->Util->ajaxReturn(false, $errors);
-            }
-        }
-//        $industries = $this->User->Industrie->find('list', ['limit' => 200]);
-//        $cities = $this->User->Cities->find('list', ['limit' => 200]);
-        $this->set(compact('user'));
-        $this->set('_serialize', ['user']);
-    }
-
-    /**
-     * Edit method
-     *
-     * @param string|null $id User id.
-     * @return \Cake\Network\Response|void Redirects on successful edit, renders view otherwise.
-     * @throws \Cake\Network\Exception\NotFoundException When record not found.
-     */
-    public function edit($id = null) {
-        $user = $this->User->get($id, [
-            'contain' => []
-        ]);
-        if ($this->request->is(['patch', 'post', 'put'])) {
-            $user = $this->User->patchEntity($user, $this->request->data);
-            if ($this->User->save($user)) {
-                $this->Flash->success(__('The user has been saved.'));
-                return $this->redirect(['action' => 'index']);
-            } else {
-                $this->Flash->error(__('The user could not be saved. Please, try again.'));
-            }
-        }
-        $industries = $this->User->Industries->find('list', ['limit' => 200]);
-        $cities = $this->User->Cities->find('list', ['limit' => 200]);
-        $this->set(compact('user', 'industries', 'cities'));
-        $this->set('_serialize', ['user']);
-    }
-
-    /**
-     * Delete method
-     *
-     * @param string|null $id User id.
-     * @return \Cake\Network\Response|null Redirects to index.
-     * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
-     */
-    public function delete($id = null) {
-        $this->request->allowMethod(['post', 'delete']);
-        $user = $this->User->get($id);
-        if ($this->User->delete($user)) {
-            $this->Flash->success(__('The user has been deleted.'));
-        } else {
-            $this->Flash->error(__('The user could not be deleted. Please, try again.'));
-        }
-        return $this->redirect(['action' => 'index']);
-    }
-
-    /**
-     * 用户登录
-     */
-    public function login() {
-        $this->set(array(
-            'pageTitle' => '登录'
-        ));
     }
 
 }
