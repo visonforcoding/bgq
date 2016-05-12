fcdef7c968d67d061a799c6dd70a4fd04bf89d46
caihuashu <caihuashu@35271882-e656-49ff-8e86-f188e04c67cb>
2016-05-12 08:49:13 +0000

    修改表字段

--- src/Doc/文章点赞表.sql
+++ src/Doc/文章点赞表.sql
@@ -4,7 +4,6 @@ CREATE TABLE `article_like` (
 	`relate_id` INT(11) NOT NULL COMMENT '点赞相关id，例：活动id或者是资讯id',
 	`create_time` DATETIME NOT NULL COMMENT '点赞时间',
 	`type` TINYINT NOT NULL COMMENT '类型值：0：活动；1：资讯',
-	`is_delete` TINYINT(2) DEFAULT 0 NOT NULL COMMENT '状态值：0：未删除；1：已删除',
 	PRIMARY KEY (`id`)
 )
 COMMENT='文章点赞表'
