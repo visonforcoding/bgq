5ab1b6e7a1264a5df45ab5fc6e21c5f2d55abbcb
caowenpeng <caowenpeng1990@126.com>
2016-06-16 08:04:12 +0000

    wx debug

--- src/Controller/Mobile/NewsController.php
+++ src/Controller/Mobile/NewsController.php
@@ -23,6 +23,7 @@ class NewsController extends AppController {
     public function index() {
         if($this->request->isWeixin()&&empty($this->user)&&!$this->request->session()->check('Login.wxbase')){
             //如果是微信 静默授权页获取openid
+            \Cake\Log\Log::debug('进行静默登陆');
             $this->loadComponent('Wx');
             $this->request->session()->delete('Login.wxbase');  //每次还是会进行静默登陆，但是不会死循环
             return $this->Wx->getUserJump(true);
