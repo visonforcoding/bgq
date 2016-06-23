f3eb3b1961533da8dcd0656a852e33a617529f1e
caowenpeng <caowenpeng1990@126.com>
2016-06-23 13:24:10 +0000

    debug app 上传

--- src/Controller/Mobile/ApiController.php
+++ src/Controller/Mobile/ApiController.php
@@ -22,7 +22,7 @@ class ApiController extends AppController {
     public function initialize() {
         parent::initialize();
     }
-    
+
     public function beforeFilter(\Cake\Event\Event $event) {
         parent::beforeFilter($event);
         return $this->checkAcl();
@@ -41,7 +41,7 @@ class ApiController extends AppController {
             if (!$this->checkSign($this->request->data())) {
                 return $this->Util->ajaxReturn(false, '验证不通过', 401);
             }
-        }else{
+        } else {
             return $this->baseCheckAcl();
         }
     }
@@ -53,13 +53,13 @@ class ApiController extends AppController {
             return $this->Util->ajaxReturn(false, '参数不正确', 412);
         }
         $timediff = time() - $timestamp;
-        if($timediff >30*60){
-            return $this->Util->ajaxReturn(false, '时间参数过期',408);
+        if ($timediff > 30 * 60) {
+            return $this->Util->ajaxReturn(false, '时间参数过期', 408);
         }
-        $sign = strtoupper(md5($timestamp.self::TOKEN));
+        $sign = strtoupper(md5($timestamp . self::TOKEN));
         \Cake\Log\Log::debug($sign);
-        if($sign!=$access_token){
-            return $this->Util->ajaxReturn(false, '验证不通过',401);
+        if ($sign != $access_token) {
+            return $this->Util->ajaxReturn(false, '验证不通过', 401);
         }
     }
 
@@ -75,17 +75,18 @@ class ApiController extends AppController {
         $sign = strtoupper(md5($stringB));
         return $sign;
     }
-    
-    protected function baseSign($time){
-        return strtoupper(md5($time.self::TOKEN));
+
+    protected function baseSign($time) {
+        return strtoupper(md5($time . self::TOKEN));
     }
 
     public function test() {
         $this->Util->ajaxReturn(['access_token' => $this->setSign($this->request->data())]);
     }
+
     public function baseTest() {
         $time = time();
-        return $this->Util->ajaxReturn(['access_token' => $this->baseSign($time),'time'=>$time]);
+        return $this->Util->ajaxReturn(['access_token' => $this->baseSign($time), 'time' => $time]);
     }
 
     /**
@@ -103,7 +104,6 @@ class ApiController extends AppController {
         return $access_token == $sign;
     }
 
-    
     /**
      * 上传接口
      * @return type
@@ -111,6 +111,13 @@ class ApiController extends AppController {
     public function upload() {
         $this->autoRender = false;
         $dir = 'app';
+        $extra_data = $this->request->data('extra_param');
+        $extra_data_json = json_decode($extra_data);
+        if(is_object($extra_data_json)){
+            if(isset($extra_data_json->dir)){
+                $dir = $extra_data_json->dir;
+            }
+        }
         $today = date('Y-m-d');
         $urlpath = '/upload/' . $dir . '/' . $today . '/';
         $savePath = ROOT . '/webroot' . $urlpath;
@@ -119,13 +126,23 @@ class ApiController extends AppController {
         $upload->allowExts = array('jpg', 'gif', 'png', 'jpeg', 'zip', 'ppt',
             'pptx', 'doc', 'docx', 'xls', 'xlsx', 'webp'); // 设置附件上传类型
         $upload->savePath = $savePath; // 设置附件上传目录
+        if(isset($extra_data_json->zip)){
+            if ($extra_data_json->zip) {
+                //缩略图处理
+                $upload->thumb = true;
+                $upload->thumbMaxWidth = '60';
+                $upload->thumbMaxHeight = '60';
+            }
+        }
+        $upload->savePath = $savePath; // 设置附件上传目录
         if (!$upload->upload()) {// 上传错误提示错误信息
             $response['status'] = false;
             $response['msg'] = $upload->getErrorMsg();
         } else {// 上传成功 获取上传文件信息
             $info = $upload->getUploadFileInfo();
             $response['status'] = true;
-            $response['path'] = $this->request->scheme() . '://' . $_SERVER['SERVER_NAME'].$urlpath . $info[0]['savename'];
+            $response['path'] = $urlpath . $info[0]['savename'];
+            $response['thumbpath'] = $urlpath . $upload->thumbPrefix . $info[0]['savename'];
             $response['msg'] = '上传成功!';
         }
         return $this->Util->ajaxReturn($response);
