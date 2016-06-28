c7129c83682016ee81efae16e481d69cf9130254
xiaofeng <xiaofeng@35271882-e656-49ff-8e86-f188e04c67cb>
2016-06-28 09:40:03 +0000

    jsapi

--- webroot/mobile/js/util.js
+++ webroot/mobile/js/util.js
@@ -227,31 +227,20 @@ $.util = {
         });
     },
     wxUploadPic:function(func){
-        alert('begin');
         wx.chooseImage({
             count: 1, // 默认9
             sizeType: ['compressed'], // ['original', 'compressed'] 可以指定是原图还是压缩图，默认二者都有
             sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
             success: function (res) {
-                alert(9999)
-                alert(JSON.stringify(res));
                 var localIds = res.localIds; // 返回选定照片的本地ID列表，localId可以作为img标签的src属性显示图片
-                alert(localIds)
-                alert(localIds[0]);
                 wx.uploadImage({
                     localId: localIds[0], // 需要上传的图片的本地ID，由chooseImage接口获得
                     isShowProgressTips: 1, // 默认为1，显示进度提示
                     success: function (res) {
-                        alert(888)
-                        alert(JSON.stringify(res));
                         var serverId = res.serverId; // 返回图片的服务器端ID
                         func && func(serverId);
                     }
                 });
-            },
-            fail: function (res) {
-                alert(777)
-                alert(JSON.stringify(res));
             }
         });
     }
