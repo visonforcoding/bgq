8c08f80b2da1b7bcf9ae144f62cec64c91fe5ebf
xiaofeng <xiaofeng@35271882-e656-49ff-8e86-f188e04c67cb>
2016-06-29 07:55:27 +0000

    jsapi

--- config/dataconf.php
+++ config/dataconf.php
@@ -7,8 +7,8 @@
  */
 return [
     'key' => [
-        'hanvon' => '4b650ea1-b39b-4dc2-b797-68f3de0dec19',
-        'juhe' => '9379ea56576d3d2c07c992afa3383f3b'
+        'hanvon' => 'b7be897a-a101-4163-88c9-d914cd9ecb59'
+        //,'juhe' => '9379ea56576d3d2c07c992afa3383f3b'
     ],
     'sms' => [
         'userid' => 14394,
