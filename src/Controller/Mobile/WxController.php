d86760f99138a9e1b288352868c8b5fd855275c4
caowenpeng <caowenpeng@35271882-e656-49ff-8e86-f188e04c67cb>
2016-06-30 02:54:39 +0000

    静默登录

--- src/Controller/Mobile/WxController.php
+++ src/Controller/Mobile/WxController.php
@@ -99,13 +99,13 @@ class WxController extends AppController {
         \Cake\Log\Log::debug($res,'devlog');
         $this->request->session()->write('Login.wxbase', true);
         $user = false;
-        if (isset($res->unionid)) {
-            $union_id = $res->unionid;
-            $user = $this->User->findByUnion_idAndEnabled($union_id,1)->first();
-        }elseif (isset($res->openid)) {
-            $open_id = $res->openid;
-            $user = $this->User->findByWx_openidAndEnabled($open_id,1)->first();
-        }
+//        if (isset($res->unionid)) {
+//            $union_id = $res->unionid;
+//            $user = $this->User->findByUnion_idAndEnabled($union_id,1)->first();
+//        }elseif (isset($res->openid)) {
+        $open_id = $res->openid;
+        $user = $this->User->findByWx_openidAndEnabled($open_id,1)->first();
+//        }
         if ($user) {
             //通过微信 获取到 在平台上有绑定的用户  就默认登录
             if(empty($user->wx_openid)){
