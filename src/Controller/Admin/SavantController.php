55b4a2b5547a1cdea15435f562d4621957b7ca24
caihuashu <caihuashu@35271882-e656-49ff-8e86-f188e04c67cb>
2016-07-07 07:12:25 +0000

    后台专家管理

---
+++ src/Controller/Admin/SavantController.php
@@ -0,0 +1,223 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use Wpadmin\Controller\AppController;
+
+/**
+ * Savant Controller
+ *
+ * @property \App\Model\Table\SavantTable $Savant
+ */
+class SavantController extends AppController {
+
+    /**
+     * Index method
+     *
+     * @return void
+     */
+    public function index() {
+        $this->set('savant', $this->Savant);
+    }
+
+    /**
+     * View method
+     *
+     * @param string|null $id Savant id.
+     * @return void
+     * @throws \Cake\Network\Exception\NotFoundException When record not found.
+     */
+    public function view($id = null) {
+        $this->viewBuilder()->autoLayout(false);
+        $savant = $this->Savant->get($id, [
+            'contain' => ['Users', 'News', 'Activity']
+        ]);
+        $this->set('savant', $savant);
+        $this->set('_serialize', ['savant']);
+    }
+
+    /**
+     * Add method
+     *
+     * @return void Redirects on successful add, renders view otherwise.
+     */
+    public function add() {
+        $savant = $this->Savant->newEntity();
+        if ($this->request->is('post')) {
+            $savant = $this->Savant->patchEntity($savant, $this->request->data);
+            if ($this->Savant->save($savant)) {
+                $this->Util->ajaxReturn(true, '添加成功');
+            } else {
+                $errors = $savant->errors();
+                $this->Util->ajaxReturn(['status' => false, 'msg' => getMessage($errors), 'errors' => $errors]);
+            }
+        }
+        $users = $this->Savant->Users->find('list', ['limit' => 200]);
+        $this->set(compact('savant', 'users'));
+    }
+
+    /**
+     * Edit method
+     *
+     * @param string|null $id Savant id.
+     * @return void Redirects on successful edit, renders view otherwise.
+     * @throws \Cake\Network\Exception\NotFoundException When record not found.
+     */
+    public function edit($id = null) {
+        $savant = $this->Savant->get($id, [
+            'contain' => ['Users']
+        ]);
+        if ($this->request->is(['post', 'put'])) {
+            $savant = $this->Savant->patchEntity($savant, $this->request->data);
+            if ($this->Savant->save($savant)) {
+                $this->Util->ajaxReturn(true, '修改成功');
+            } else {
+                $errors = $savant->errors();
+                $this->Util->ajaxReturn(false, getMessage($errors));
+            }
+        }
+        $users = $this->Savant->Users->find('list', ['limit' => 200]);
+        $selUserIds = [];
+        if($savant->user)
+        {
+            $selUserIds[] = $savant->user->id;
+        }
+        $this->set(compact('savant', 'users', 'selUserIds'));
+    }
+
+    /**
+     * Delete method
+     *
+     * @param string|null $id Savant id.
+     * @return \Cake\Network\Response|null Redirects to index.
+     * @throws \Cake\Network\Exception\NotFoundException When record not found.
+     */
+    public function delete($id = null) {
+        $this->request->allowMethod('post');
+        $id = $this->request->data('id');
+        if ($this->request->is('post')) {
+            $savant = $this->Savant->get($id);
+            if ($this->Savant->delete($savant)) {
+                $this->Util->ajaxReturn(true, '删除成功');
+            } else {
+                $errors = $savant->errors();
+                $this->Util->ajaxReturn(true, getMessage($errors));
+            }
+        }
+    }
+
+    /**
+     * get jqgrid data 
+     *
+     * @return json
+     */
+    public function getDataList() {
+        $this->request->allowMethod('ajax');
+        $page = $this->request->data('page');
+        $rows = $this->request->data('rows');
+        $sort = 'Savant.' . $this->request->data('sidx');
+        $order = $this->request->data('sord');
+        $keywords = $this->request->data('keywords');
+        $begin_time = $this->request->data('begin_time');
+        $end_time = $this->request->data('end_time');
+        $where = [];
+        if (!empty($keywords)) {
+            $where[' Users.truename like'] = "%$keywords%";
+        }
+        if (!empty($begin_time) && !empty($end_time)) {
+            $begin_time = date('Y-m-d', strtotime($begin_time));
+            $end_time = date('Y-m-d', strtotime($end_time));
+            $where['and'] = [['date(`create_time`) >' => $begin_time], ['date(`create_time`) <' => $end_time]];
+        }
+        $query = $this->Savant->find()->contain(['Users']);
+        $query->hydrate(false);
+        if (!empty($where)) {
+            $query->where($where);
+        }
+        $nums = $query->count();
+        if (!empty($sort) && !empty($order)) {
+            $query->order([$sort => $order]);
+        }
+
+        $query->limit(intval($rows))
+                ->page(intval($page));
+        $res = $query->toArray();
+        if (empty($res)) {
+            $res = array();
+        }
+        if ($nums > 0) {
+            $total_pages = ceil($nums / $rows);
+        } else {
+            $total_pages = 0;
+        }
+        $data = array('page' => $page, 'total' => $total_pages, 'records' => $nums, 'rows' => $res);
+        $this->autoRender = false;
+        $this->response->type('json');
+        echo json_encode($data);
+    }
+
+    /**
+     * export csv
+     *
+     * @return csv 
+     */
+    public function exportExcel() {
+        $sort = $this->request->data('sidx');
+        $order = $this->request->data('sord');
+        $keywords = $this->request->data('keywords');
+        $begin_time = $this->request->data('begin_time');
+        $end_time = $this->request->data('end_time');
+        $where = [];
+        if (!empty($keywords)) {
+            $where[' username like'] = "%$keywords%";
+        }
+        if (!empty($begin_time) && !empty($end_time)) {
+            $begin_time = date('Y-m-d', strtotime($begin_time));
+            $end_time = date('Y-m-d', strtotime($end_time));
+            $where['and'] = [['date(`ctime`) >' => $begin_time], ['date(`ctime`) <' => $end_time]];
+        }
+        $Table = $this->Savant;
+        $column = ['user_id', '推荐次数', '封面', '项目经验', '资源优势', '简洁'];
+        $query = $Table->find();
+        $query->hydrate(false);
+        $query->select(['user_id', 'reco_nums', 'cover', 'xmjy', 'zyys', 'summary']);
+        if (!empty($where)) {
+            $query->where($where);
+        }
+        if (!empty($sort) && !empty($order)) {
+            $query->order([$sort => $order]);
+        }
+        $res = $query->toArray();
+        $this->autoRender = false;
+        $filename = 'Savant_' . date('Y-m-d') . '.csv';
+        \Wpadmin\Utils\Export::exportCsv($column, $res, $filename);
+    }
+    
+    public function pass($id){
+        $savant = $this->Savant->get($id);
+        $userTable = \Cake\ORM\TableRegistry::get('user');
+        $user = $userTable->get($savant->user_id);
+        $user->level = 2;
+        $user->savant_status = 3;
+        $res = $userTable->save($user);
+        if($res){
+            return $this->Util->ajaxReturn(true, '审核通过');
+        } else {
+            return $this->Util->ajaReturn(false, '系统错误');
+        }
+    }
+    
+    public function unpass($id){
+        $savant = $this->Savant->get($id);
+        $userTable = \Cake\ORM\TableRegistry::get('user');
+        $user = $userTable->get($savant->user_id);
+        $user->savant_status = 0;
+        $res = $userTable->save($user);
+        if($res){
+            return $this->Util->ajaxReturn(true, '审核不通过');
+        } else {
+            return $this->Util->ajaReturn(false, '系统错误');
+        }
+    }
+
+}
