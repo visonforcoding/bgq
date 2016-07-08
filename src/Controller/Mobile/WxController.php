f5bc5ff51451fe74995e31d32ca8592505db49d8
caowenpeng <caowenpeng1990@126.com>
2016-07-08 11:08:53 +0000

    debug 支付宝支付

--- src/Controller/Mobile/WxController.php
+++ src/Controller/Mobile/WxController.php
@@ -135,18 +135,17 @@ class WxController extends AppController {
             
         }
         //$fee = intval(($order->price)*100);  //支付金额(分)
-        $fee = 1;  //测试时 1分
+        $fee = 0.01; //元
         $this->loadComponent('Wxpay');
         $isApp = false;
         $aliPayParameters = '';
-        $jsApiParameters = $this->Wxpay->getPayParameter($body, $openid, $out_trade_no, $fee,null,$isApp);
         if($this->request->is('lemon')){
             $isApp = true;
             $openid = $this->user->app_wx_openid;
             $this->loadComponent('Alipay');
-            $fee = $fee/100;  //转化成元
             $aliPayParameters = $this->Alipay->setPayParameter($out_trade_no, '并购帮-预约话题', $fee, $body);
         }
+        $jsApiParameters = $this->Wxpay->getPayParameter($body, $openid, $out_trade_no, $fee,null,$isApp);
         $this->set(array(
             'jsApiParameters' => $jsApiParameters,
             'isWx'=>  $this->request->is('weixin')?true:false,
