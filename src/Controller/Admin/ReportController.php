3c2bd5243ed2dafee2c09d88ca3203ded76ee2ad
caowenpeng <caowenpeng1990@126.com>
2016-08-22 07:04:58 +0000

    上报错误

--- src/Controller/Admin/ReportController.php
+++ src/Controller/Admin/ReportController.php
@@ -29,7 +29,7 @@ class ReportController extends AppController {
         $pv = $PvlogTable->newEntity($data);
         $pv->ip = $this->request->clientIp();
         $pv->url = $this->request->referer();
-        $user = $this->request->session('User.mobile');
+        $user = $this->request->session()->read('User.mobile');
         $pv->user_id = $user->id;
         $pv->useragent =  $this->request->header('User-Agent');
         $PvlogTable->save($pv);
