de912fcb9cae0c8c9c75d36c1832a409643ba4c5
xiaofeng <xiaofeng@35271882-e656-49ff-8e86-f188e04c67cb>
2016-08-31 03:29:22 +0000

    ci

--- webroot/mobile/js/util.js
+++ webroot/mobile/js/util.js
@@ -262,12 +262,12 @@ $.util = {
         return !!$.util.getCookie('login_status');
     },
     staticLogin:function(){
-        if($.util.isAPP){
+        //if($.util.isAPP){
             var ifr = document.createElement('iframe');
             ifr.style.display = 'none';
             ifr.src = '/user/login_status';
             document.body.appendChild(ifr);
-        }
+        //}
     },
     isMobile:function(str) {
         var reg = /^0?1[3|4|5|7|8][0-9]\d{8}$/;
