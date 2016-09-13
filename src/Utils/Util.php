8274176b408a9ca9593e7a8286cc563c250403fe
caowenpeng <caowenpeng1990@126.com>
2016-09-13 07:50:21 +0000

    后台

--- src/Utils/Util.php
+++ src/Utils/Util.php
@@ -5,8 +5,7 @@
  * Created on   :   2015-5-22 17:19:18 by 曹文鹏 , caowenpeng1990@126.com
  */
 
-namespace Wpadmin\Utils;
-
+namespace App\Utils;
 class Util {
 
     public static function getMenu($list, $pid_key = 'pid', $id_key = 'id') {
@@ -114,6 +113,37 @@ class Util {
             return $filedata;
         }
         return FALSE;
+    }
+
+    public static function get_chars($utf8_str) {
+        $s = $utf8_str;
+        $len = strlen($s);
+        if ($len == 0)
+            return array();
+        $chars = array();
+        for ($i = 0; $i < $len; $i++) {
+            $c = $s[$i];
+            $n = ord($c);
+            if (($n >> 7) == 0) {       //0xxx xxxx, asci, single
+                $chars[] = $c;
+            } else if (($n >> 4) == 15) {     //1111 xxxx, first in four char
+                if ($i < $len - 3) {
+                    $chars[] = $c . $s[$i + 1] . $s[$i + 2] . $s[$i + 3];
+                    $i += 3;
+                }
+            } else if (($n >> 5) == 7) {  //111x xxxx, first in three char
+                if ($i < $len - 2) {
+                    $chars[] = $c . $s[$i + 1] . $s[$i + 2];
+                    $i += 2;
+                }
+            } else if (($n >> 6) == 3) {  //11xx xxxx, first in two char
+                if ($i < $len - 1) {
+                    $chars[] = $c . $s[$i + 1];
+                    $i++;
+                }
+            }
+        }
+        return $chars;
     }
 
 }
