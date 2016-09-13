3505c3232f28f0ba37aa5e806737aaccacd1f288
caowenpeng <caowenpeng@35271882-e656-49ff-8e86-f188e04c67cb>
2016-09-13 03:17:00 +0000

    后台

---
+++ src/Model/Table/WordTable.php
@@ -0,0 +1,50 @@
+<?php
+namespace App\Model\Table;
+
+use App\Model\Entity\Word;
+use Cake\ORM\Query;
+use Cake\ORM\RulesChecker;
+use Cake\ORM\Table;
+use Cake\Validation\Validator;
+
+/**
+ * Word Model
+ *
+ */
+class WordTable extends Table
+{
+
+    /**
+     * Initialize method
+     *
+     * @param array $config The configuration for the Table.
+     * @return void
+     */
+    public function initialize(array $config)
+    {
+        parent::initialize($config);
+
+        $this->table('word');
+        $this->displayField('id');
+        $this->primaryKey('id');
+    }
+
+    /**
+     * Default validation rules.
+     *
+     * @param \Cake\Validation\Validator $validator Validator instance.
+     * @return \Cake\Validation\Validator
+     */
+    public function validationDefault(Validator $validator)
+    {
+        $validator
+            ->integer('id')
+            ->allowEmpty('id', 'create');
+
+        $validator
+            ->requirePresence('body', 'create')
+            ->notEmpty('body');
+
+        return $validator;
+    }
+}
