b98cb527ae9570792c751b3f43c7bbf983df3673
caowenpeng <caowenpeng@35271882-e656-49ff-8e86-f188e04c67cb>
2016-09-28 03:54:41 +0000

    更改

--- src/Controller/Admin/PvchartController.php
+++ src/Controller/Admin/PvchartController.php
@@ -118,7 +118,7 @@ class PvchartController extends AppController {
         $end_time = $this->request->data('end_time');
         $where = [];
         if (!empty($keywords)) {
-            $where['url like'] = "%$keywords%";
+            $where['or'] = [['url like' => "%$keywords%"], ['ptag like' => "%$keywords%"]];
         }
         if (!empty($begin_time) && !empty($end_time)) {
             $begin_time = date('Y-m-d', strtotime($begin_time));
